2128fc80921867397c992854850f263fa222cf24
rodrigovalest <rodrigo.vale.st@email.com>
2023-11-01 11:34:17 -0300

    feat: dropping words (to fix)

--- script.js
+++ script.js
@@ -7,15 +7,31 @@ function getRandomInt(min, max) {
 let c = document.getElementById("gameboard");
 let ctx = c.getContext("2d");
 
+const words = [
+  "abacate",
+  "amor",
+  "amigo",
+  "animal",
+  "ano",
+  "azul",
+  "casa",
+  "celular",
+  "cor",
+  "dia",
+  "emprego",
+  "escola",
+  "espada",
+  "festa",
+  "floresta",
+  "fogo",
+  "frio",
+  "gato",
+  "homem",
+  "mulher"  
+];
 const boxes = [];
 let tempo = 0;
 
-const character = {
-  x: 300,
-  y: 560,
-  default: true
-}
-ctx.fillRect(character.x, character.y, 40, 40);
 
 function generateDropping() {
   // Para cada caixa no array, apagar o quadrado antigo e printar um novo
@@ -24,41 +40,35 @@ function generateDropping() {
     if (box.y > 600) {
       boxes.splice(index, 1);
     }
-
-    // Mover character para a ultima caixa caindo
-    if (index == 0 && box.y > 400) {
-      ctx.clearRect(character.x, character.y, 40, 40);
-      character.y = box.y - 40;
-      character.x = box.x + 30;
-      ctx.fillRect(character.x, character.y, 40, 40);
-    }
   
-    ctx.clearRect(box.x, box.y, 100, 20);
+    ctx.clearRect(box.x, box.y, box.wordWidth, -40);
     box.y++;
-    ctx.fillRect(box.x, box.y, 100, 20);
+    ctx.fillText(box.word, box.x, box.y);
   });
 
   // Gerar novo bloco no lado direito sempre quando o tempo for 100
   if (tempo == 100) {
     const x = getRandomInt(330, 480);
     let y = 0;
-    ctx.fillRect(x, y, 100, 20);
-    boxes.push({ x, y });
+    let word = "hello";
+    ctx.fillStyle = "black";
+    ctx.font = "16px Arial";
+    ctx.fillText(word, x, y);
+    let wordWidth = ctx.measureText(word).width;
+    boxes.push({ x, y, wordWidth, word });
   }
 
   // Gerar novo bloco no lado esquerdo sempre quando o tempo for 200 e resetar o tempo
   if (tempo == 200) {
     const x = getRandomInt(30, 200);
     let y = 0;
-    ctx.fillRect(x, y, 100, 20);
-    boxes.push({ x, y });
+    let word = "hello";
+    ctx.fillStyle = "black";
+    ctx.font = "16px Arial";
+    ctx.fillText(word, x, y);
+    let wordWidth = ctx.measureText(word).width;
+    boxes.push({ x, y, wordWidth, word });
     tempo = 0;
-  }
-
-  if (boxes[0]?.y < 50) {
-    if (character.default == false) {
-      box
-    }
   }
 
   tempo++;
