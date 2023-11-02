6cd945ca98b5695e11f3f73f83fccc4e9f2b9e0a
rodrigovalest <rodrigo.vale.st@email.com>
2023-11-01 22:27:08 -0300

    feat: keyword event, character dropping in typed words

--- script.js
+++ script.js
@@ -4,35 +4,105 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+const words = [
+  "abacaxi",
+  "bala",
+  "carro",
+  "dedo",
+  "elefante",
+  "faca",
+  "gato",
+  "hamburguer",
+  "igreja",
+  "jornal",
+  "kilo",
+  "limao",
+  "maca",
+  "nave",
+  "oculos",
+  "pato",
+  "queijo",
+  "rato",
+  "sapato",
+  "tijolo",
+  "uva",
+  "vela",
+  "waffle",
+  "xadrez",
+  "yoga",
+  "zebra",
+  "amigo",
+  "bolo",
+  "cavalo",
+  "dente",
+  "eletrico",
+  "foguete",
+  "garfo",
+  "hotel",
+  "jacare",
+  "kiwi",
+  "lampada",
+  "mochila",
+  "navio",
+  "orquidea",
+];
+
 let c = document.getElementById("gameboard");
 let ctx = c.getContext("2d");
 
-const droppingWords = [];
-let tempo = 0;
+const wCanvas = c.width;
+const hCanvas = c.height;
 
+
+const droppingWords = [];
+let gameOver = false;
+let lastTypedWord;
+let tempo = 0;
 const character = {
-  x: 300,
-  y: 560
+  x: wCanvas / 2,
+  y: hCanvas - 40,
+  playing: false
 }
 ctx.fillRect(character.x, character.y, 40, 40);
 
 function generateDropping() {
+  if (gameOver) {
+    ctx.clearRect(0, 0, wCanvas, hCanvas);
+    return;
+  }
+
+  // Para cada palavra caindo...
   droppingWords.forEach((word, index) => {
-    if (word.y > 630) {
+    // Remover palavra da array se ela chegar no final
+    if (word.y > (hCanvas + 40)) {
       droppingWords.splice(index, 1);
     }
 
-    if (index == 0 && word.y > 400) {
+    // Terminar o jogo
+    if ((character.y >= (hCanvas - 40) && character.playing) || (index == 0 && word.y >= (hCanvas - 10))) {
+      droppingWords.length = 0;
+      character.playing = false;
+      tempo = 0;
+      countLetter = 0;
+      lastTypedWord = null;
+      gameOver = true;
+
+      ctx.clearRect(0, 0, wCanvas, hCanvas);
+    }
+
+    // Fazer personagem acompanhar a queda da ultima palavra digitada
+    if (word == lastTypedWord) {
       ctx.clearRect(character.x, character.y, 40, 40);
       character.y = word.y - 65;
       character.x = word.x - 10;
       ctx.fillRect(character.x, character.y, 40, 40);
     }
   
-    ctx.clearRect(word.x - 10, word.y, word.width + 20, -25);
+    // Fazer queda da palavra (e sua caixa cinza ou azul caso já tenha sido digitada)
+    ctx.clearRect(word.x - 10, word.y + 10, word.width + 25, -35);
     word.y++;
-
-    ctx.fillStyle = "gray";
+    
+    word.typed == false ? ctx.fillStyle = "gray" : ctx.fillStyle = "blue"; 
     ctx.fillRect(word.x - 10, word.y + 10, word.width + 20, -35);
 
     ctx.fillStyle = "black";
@@ -40,30 +110,27 @@ function generateDropping() {
     ctx.fillText(word.word, word.x, word.y);
   });
 
-  if (tempo == 100) {
-    const x = getRandomInt(330, 480);
+  // Gerar novas palavras (nos dois lados)
+  if (tempo == 100 || tempo == 200) {
+    let x;
+
+    if (tempo == 100) {
+      x = getRandomInt(330, 480);
+    } else {
+      x = getRandomInt(30, 200);
+      tempo = 0;
+    }
     let y = 0;
-    let word = "hello";
+    let word = words[getRandomInt(0, words.length - 1)];;
+    
     ctx.fillStyle = "black";
     ctx.font = "16px Arial";
     ctx.fillText(word, x, y);
+    
     let width = ctx.measureText(word).width;
-    droppingWords.push({ x, y, width, word });
-
-    ctx.fillStyle = "gray";
-    ctx.fillRect(x - 10, y, width + 20, -15);
-  }
-
-  if (tempo == 200) {
-    const x = getRandomInt(30, 200);
-    let y = 0;
-    let word = "hello";
-    ctx.fillStyle = "black";
-    ctx.font = "16px Arial";
-    ctx.fillText(word, x, y);
-    let width = ctx.measureText(word).width;
-    droppingWords.push({ x, y, width, word });
-    tempo = 0;
+    let typed = false;
+    
+    droppingWords.push({ x, y, width, word, typed });
 
     ctx.fillStyle = "gray";
     ctx.fillRect(x - 10, y, width + 20, -15);
@@ -74,3 +141,36 @@ function generateDropping() {
 }
 
 generateDropping();
+
+// Evento de detecção de letras digitadas no teclado
+// Verifica a última palavra que ainda não foi digitada (letra por letra, em ordem)
+let countLetter = 0;
+document.addEventListener("keydown", function (event) {
+  const keyPressed = event.key.toLowerCase();
+
+  if (droppingWords.length > 0) {
+    let currentWord;
+
+    for (let i = 0; i < droppingWords.length; i++) {
+      if (!droppingWords[i].typed) {
+        currentWord = droppingWords[i];
+        break;
+      }
+    }
+
+    if (currentWord) {
+      const nextLetter = currentWord.word.charAt(countLetter);
+
+      if (keyPressed === nextLetter) {
+        countLetter++;
+
+        if (countLetter === currentWord.word.length) {
+          countLetter = 0;
+          currentWord.typed = true;
+          lastTypedWord = currentWord;
+          character.playing = true;
+        }
+      }
+    }
+  }
+});
