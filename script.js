45be10742f6e171c0c315504561d60e432286db5
Matea <matheusnsantana@gmail.com>
2023-11-05 23:41:59 -0300

    [Add] Sprite wizard [Mod]Scripst - Add Animation Function

--- script.js
+++ script.js
@@ -4,71 +4,179 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-let img = new Image();
-img.src = 'css_sprites.png';
-img.onload = function() {
-  init();
-};
+const words = [
+  "abacaxi",
+  "bala",
+  "carro",
+  "dedo",
+  "elefante",
+  "faca",
+  "gato",
+  "hamburguer",
+  "igreja",
+  "jornal",
+  "kilo",
+  "limao",
+  "maca",
+  "nave",
+  "oculos",
+  "pato",
+  "queijo",
+  "rato",
+  "sapato",
+  "tijolo",
+  "uva",
+  "vela",
+  "waffle",
+  "xadrez",
+  "yoga",
+  "zebra",
+  "amigo",
+  "bolo",
+  "cavalo",
+  "dente",
+  "eletrico",
+  "foguete",
+  "garfo",
+  "hotel",
+  "jacare",
+  "kiwi",
+  "lampada",
+  "mochila",
+  "navio",
+  "orquidea",
+];
+
+
 
 let c = document.getElementById("gameboard");
 let ctx = c.getContext("2d");
 
-function init() {
-  ctx.drawImage(img, 0, 0, 120, 120, 10, 10, 120, 180);
+const img = new Image();
+const scale = 0.3;
+const spriteWidth = 256;
+const spriteHeight = 256;
+const scaledWidth = scale * spriteWidth;
+const scaledHeight = scale * spriteHeight;
+img.src = "SpriteWizard.png";
+img.onload = function() {
+  init();
+};
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight,
+                canvasX, canvasY, scaledWidth, scaledHeight);
 }
 
-const boxes = [];
-let tempo = 0;
 
+const cycleLoop = [0,1,2,1];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step() {
+  frameCount++;
+  if (frameCount < 3) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, wCanvas, hCanvas);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+function init() {
+  window.requestAnimationFrame(step);
+}
+
+const wCanvas = c.width;
+const hCanvas = c.height;
+
+
+const droppingWords = [];
+let gameOver = false;
+let lastTypedWord;
+let tempo = 0;
 const character = {
-  x: 300,
-  y: 560,
-  default: true
+  x: wCanvas / 2,
+  y: hCanvas - 40,
+  playing: false
 }
 ctx.fillRect(character.x, character.y, 40, 40);
 
 function generateDropping() {
-  // Para cada caixa no array, apagar o quadrado antigo e printar um novo
-  boxes.forEach((box, index) => {
-    // Remover a primeira box dentro de boxes, quando ela chegar no final da página (DETECTAR COLISÃO AQUI)
-    if (box.y > 600) {
-      boxes.splice(index, 1);
+  if (gameOver) {
+    ctx.clearRect(0, 0, wCanvas, hCanvas);
+    return;
+  }
+
+  // Para cada palavra caindo...
+  droppingWords.forEach((word, index) => {
+    // Remover palavra da array se ela chegar no final
+    if (word.y > (hCanvas + 40)) {
+      droppingWords.splice(index, 1);
     }
 
-    // Mover character para a ultima caixa caindo
-    if (index == 0 && box.y > 400) {
+    // Terminar o jogo
+    if ((character.y >= (hCanvas - 40) && character.playing) || (index == 0 && word.y >= (hCanvas - 10))) {
+      droppingWords.length = 0;
+      character.playing = false;
+      tempo = 0;
+      countLetter = 0;
+      lastTypedWord = null;
+      gameOver = true;
+
+      ctx.clearRect(0, 0, wCanvas, hCanvas);
+    }
+
+    // Fazer personagem acompanhar a queda da ultima palavra digitada
+    if (word == lastTypedWord) {
       ctx.clearRect(character.x, character.y, 40, 40);
-      character.y = box.y - 40;
-      character.x = box.x + 30;
+      character.y = word.y - 65;
+      character.x = word.x - 10;
       ctx.fillRect(character.x, character.y, 40, 40);
     }
-  
-    ctx.clearRect(box.x, box.y, 100, 20);
-    box.y++;
-    ctx.fillRect(box.x, box.y, 100, 20);
+
+    // Fazer queda da palavra (e sua caixa cinza ou azul caso já tenha sido digitada)
+    ctx.clearRect(word.x - 10, word.y + 10, word.width + 25, -35);
+    word.y++;
+
+    word.typed == false ? ctx.fillStyle = "gray" : ctx.fillStyle = "blue";
+    ctx.fillRect(word.x - 10, word.y + 10, word.width + 20, -35);
+
+    ctx.fillStyle = "black";
+    ctx.font = "16px Arial";
+    ctx.fillText(word.word, word.x, word.y);
   });
 
-  // Gerar novo bloco no lado direito sempre quando o tempo for 100
-  if (tempo == 100) {
-    const x = getRandomInt(330, 480);
-    let y = 0;
-    ctx.fillRect(x, y, 100, 20);
-    boxes.push({ x, y });
-  }
+  // Gerar novas palavras (nos dois lados)
+  if (tempo == 100 || tempo == 200) {
+    let x;
 
-  // Gerar novo bloco no lado esquerdo sempre quando o tempo for 200 e resetar o tempo
-  if (tempo == 200) {
-    const x = getRandomInt(30, 200);
-    let y = 0;
-    ctx.fillRect(x, y, 100, 20);
-    boxes.push({ x, y });
-    tempo = 0;
-  }
-
-  if (boxes[0]?.y < 50) {
-    if (character.default == false) {
-      box
+    if (tempo == 100) {
+      x = getRandomInt(330, 480);
+    } else {
+      x = getRandomInt(30, 200);
+      tempo = 0;
     }
+    let y = 0;
+    let word = words[getRandomInt(0, words.length - 1)];;
+
+    ctx.fillStyle = "black";
+    ctx.font = "16px Arial";
+    ctx.fillText(word, x, y);
+
+    let width = ctx.measureText(word).width;
+    let typed = false;
+
+    droppingWords.push({ x, y, width, word, typed });
+
+    ctx.fillStyle = "gray";
+    ctx.fillRect(x - 10, y, width + 20, -15);
   }
 
   tempo++;
@@ -76,3 +184,36 @@ function generateDropping() {
 }
 
 generateDropping();
+
+// Evento de detecção de letras digitadas no teclado
+// Verifica a última palavra que ainda não foi digitada (letra por letra, em ordem)
+let countLetter = 0;
+document.addEventListener("keydown", function (event) {
+  const keyPressed = event.key.toLowerCase();
+
+  if (droppingWords.length > 0) {
+    let currentWord;
+
+    for (let i = 0; i < droppingWords.length; i++) {
+      if (!droppingWords[i].typed) {
+        currentWord = droppingWords[i];
+        break;
+      }
+    }
+
+    if (currentWord) {
+      const nextLetter = currentWord.word.charAt(countLetter);
+
+      if (keyPressed === nextLetter) {
+        countLetter++;
+
+        if (countLetter === currentWord.word.length) {
+          countLetter = 0;
+          currentWord.typed = true;
+          lastTypedWord = currentWord;
+          character.playing = true;
+        }
+      }
+    }
+  }
+});
