8f0039261dee741e29cbac17c45370048751eb6d
Matea <matheusnsantana@gmail.com>
2023-11-29 16:08:47 -0300

    [Working]ScoreBoard

--- GlobalScript.js
+++ GlobalScript.js
@@ -1,3 +1,14 @@
+let isTvOn = true;
 function TurnTv() {
-    window.location.href = "../../index.html";
+    const title = document.getElementsByClassName('titleScreen');
+    if (isTvOn) {
+        for (let index = 0; index < title.length; index++) {
+            title[index].style.visibility = 'hidden';
+        }
+        document.getElementById('powerButton').style.backgroundColor = "red";
+        document.getElementById('powerButton').style.borderColor = "darkred";
+        isTvOn = false;
+    } else {
+        window.location.href = "../../index.html";
+    }
 }
